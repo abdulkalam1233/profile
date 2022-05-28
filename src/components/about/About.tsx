fff945e26d71235dfa4f063086e0b2d484ecc099
abdulkalam1233 <abdulkalamshaik1233@gmail.com>
2022-05-28 14:26:10 +0530

    ENH: Added skills

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -8,6 +8,14 @@ import {
 } from "react-icons/fa";
 import profile from "../../assets/profile.png";
 import Resume from "../../assets/resume.pdf";
+import {
+  DiJava,
+  DiJavascript,
+  DiMongodb,
+  DiMysql,
+  DiNodejs,
+  DiReact,
+} from "react-icons/di";
 
 function About() {
   return (
@@ -18,8 +26,15 @@ function About() {
       <div className="name text-4xl sm:text-2xl font-bold m-2 self-center">
         Abdul Kalam
       </div>
-      <div className="designation text-xl text-bold text-slate-500 m-2">
-        Software CraftsPerson
+      <div className="designation text-xl text-bold text-slate-500 m-2 flex flex-col">
+        <p>Software CraftsPerson</p>
+        {/* <a href="https://www.incubyte.co/" className="self-center mt-2">
+          <img
+            src="https://uploads-ssl.webflow.com/5ea96576effbba77b11132b4/5f34c4a0e68d1a0ca324c134_Incubyte%20Final%20Logo-01.svg"
+            alt="Incubyte"
+            width={"100px"}
+          />
+        </a> */}
       </div>
       <div className="network m-2 flex space-x-3">
         <a href="https://github.com/abdulkalam1233" target="_blank">
@@ -41,7 +56,7 @@ function About() {
           <FaInstagram size={"1.5rem"} />
         </a>
       </div>
-      <div className="resume mt-2">
+      <div className="resume mt-4">
         <a
           className="bg-contain bg-red-500 p-2 rounded-lg text-white w-[6rem]"
           href={Resume}
@@ -49,6 +64,14 @@ function About() {
         >
           Resume
         </a>
+      </div>
+      <div className="network mt-6 flex space-x-3">
+        <DiNodejs size={"2rem"} color="green" title="NodeJS" />
+        <DiReact size={"2rem"} color="skyblue" title="React" />
+        <DiMysql size={"2rem"} color="skyblue" title="MySQL" />
+        <DiMongodb size={"2rem"} color="green" title="MongoDB" />
+        <DiJava size={"2rem"} color="brown" title="Java" />
+        <DiJavascript size={"2rem"} color="blue" title="JavaScript" />
       </div>
     </div>
   );
